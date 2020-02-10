0f69bc57d26b71a97ac8214149ce50a92a998718
Mark B <59287626+MarkBuckwell@users.noreply.github.com>
2020-02-10 17:54:29 +0000

    Added additional import functions.
    Function to show only .txt files for import.
    Function to import CSVs to list of DataFrames.
    Working on function to convert list of lists to 3D matrix of data.

--- MyFunctions.py
+++ MyFunctions.py
@@ -7,25 +7,69 @@ Created on Tue Jan  7 14:17:33 2020
 """
 
 # A file to store functions that I'd like to use regularly.
-#import os
-import numpy as np
-import tkinter # Used for GetFileAddresses, 
+import os # To clear console and variables.
+import numpy as np # Used for ConfigureColumns
+import pandas as pd # Used for ReadCSVsToList.
+import matplotlib.pyplot as plt # Used for CloseAllPlots.
+import tkinter # Used for GetFileAddresses and GetTxtAddresses.
 from tkinter import filedialog # Used for GetFileAddresses
-
-# Get current working directory.
-# Location = os.getcwd()
 
 # Open a dialog for the user to select one or multiple files.
 def GetFileAddresses():
     tkinter.Tk().withdraw() # Prevent root winow from appearing.
     FileAddresses = filedialog.askopenfilenames()
-    return (FileAddresses)
+    return(FileAddresses)
 
-# Read ascii data from a tuple of file addresses and output an array.
-def ReadTxtDataFiles(FileAddresses):
-    NumFiles = len(FileAddresses)
-    DataArray = np.array(("TEXT",1,NumFiles))
-    for FileNumber in range(NumFiles):
-        CurrentFile = open(FileAddresses[FileNumber], 'r')
-        DataArray[FileNumber] = CurrentFile.readlines()
-    return (DataArray)
+# Open a dialog for the user to select one or multiple .txt files only.
+def GetTxtAddresses():
+    tkinter.Tk().withdraw() # Prevent root winow from appearing.
+    TxtAddresses = filedialog.askopenfilenames(filetypes=(('text files', 'txt'),))
+    return(TxtAddresses)
+
+# Read ascii data from a tuple of file addresses and output a list of lists.
+# Data format not important at this point, just getting all the data imported.
+def ReadTxtFilesToList(FileAddresses):
+    OutputList = [open(
+            FileAddresses[FileNumber], 'r', errors='ignore') for FileNumber in range(
+                len(FileAddresses))]
+    return(OutputList)
+
+def ReadCSVsToList(FileAddresses, Delimiter, HeaderRows):
+    OutputList = [pd.read_csv(FileAddresses[FileNumber],
+                              sep = Delimiter,
+                              header = HeaderRows,
+                              engine = 'python'
+                              ) for FileNumber in range(len(FileAddresses))]
+    return(OutputList)
+
+# Separate out list of lists into separate matrices of numerical data.
+# Where each item in the list corresponds to the contents of a different file.
+# Can define the number of header rows to ignore.
+def ListsToMatrices(OutputList, HeaderRows):
+    for FileNumber in range(len(OutputList)):
+        TransferList = OutputList[FileNumber]  # Make a new list to transfer data.
+        TransferArray = pd.DataFrame() # Array to handle data for each file.
+        DataList = []
+        HeaderRows = 0 
+        for DataRow in range(len(TransferList) - HeaderRows):
+            # Split out row from list item, making new row of strings.
+            TransferRow = TransferList[DataRow + HeaderRows].split()
+            for DataColumn in range(len(TransferRow)):
+                TransferRow[DataColumn] = float(TransferRow[DataColumn])
+            TransferArray = TransferArray.append(
+                TransferRow, ignore_index = True)
+        DataList[FileNumber] = TransferArray
+    return()
+            
+# Close all plots.
+def CloseAllPlots():
+    plt.close('all')
+    plt.close()
+    
+# CLear the console window.
+def ClearConsole():
+    clear = lambda: os.system('cls')
+    clear()
+     
+# Get current working directory.
+# Location = os.getcwd()
